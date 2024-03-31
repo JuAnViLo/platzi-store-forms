f42fa4c76565320d69179c110f467a616714554d
Juan Villada <juanandresvl14@gmail.com>
2024-03-31 16:19:23 -0500

    Curso Done

---
+++ src/app/home/components/search/search.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { map, debounceTime } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-search',
+  templateUrl: './search.component.html',
+  styleUrls: ['./search.component.scss'],
+})
+export class SearchComponent implements OnInit {
+  searchField = new FormControl();
+  restults: any[] = [];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.searchField.valueChanges.pipe(debounceTime(300)).subscribe((value) => {
+      this.getData(value);
+    });
+  }
+
+  private getData(query: string) {
+    const API = 'ypfhz6VpbE64DsDU4uW4eFDN2T0iihGi';
+    this.http
+      .get(
+        `https://api.giphy.com/v1/gifs/search?q=${query}&api_key=${API}&limit=12`
+      )
+      .pipe(
+        map((response: any) => {
+          return response.data.map((item) => item.images.downsized);
+        })
+      )
+      .subscribe((data) => {
+        this.restults = data;
+      });
+  }
+}
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,21 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { BannerComponent } from './components/banner/banner.component';
 import { HomeComponent } from './components/home/home.component';
-
+import { ReactiveFormsModule } from '@angular/forms';
 import { HomeRoutingModule } from './home-routing.module';
 import { SharedModule } from './../shared/shared.module';
+import { SearchComponent } from './components/search/search.component';
 
 @NgModule({
-  declarations: [
-    BannerComponent,
-    HomeComponent
-  ],
-  imports: [
-    CommonModule,
-    SharedModule,
-    HomeRoutingModule
-  ]
+  declarations: [BannerComponent, HomeComponent, SearchComponent],
+  imports: [CommonModule, SharedModule, HomeRoutingModule, ReactiveFormsModule],
 })
-export class HomeModule {
-
-}
+export class HomeModule {}
